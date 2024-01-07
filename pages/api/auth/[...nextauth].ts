f6111e447ca0b3d9b32434e2b3b3f7e2975525f2
yuximing <93099324+yuximing@users.noreply.github.com>
2024-01-07 03:16:08 -0500

    vercel fix attempt 1

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,17 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+import NextAuth from 'next-auth';
+import GitHub from 'next-auth/providers/github';
 
-export const authOptions: NextAuthOptions = {
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
   providers: [
-    GithubProvider({
+    GitHub({
       clientId: process.env.OAUTH_CLIENT_KEY as string,
       clientSecret: process.env.OAUTH_CLIENT_SECRET as string,
     }),
   ],
-};
-
-export default NextAuth(authOptions);
+  pages: {
+    signIn: '/sign-in',
+  },
+});
