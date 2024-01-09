de8e30d9a0f6a5484331d4693f5d1da341ae15d6
yuximing <93099324+yuximing@users.noreply.github.com>
2024-01-08 23:39:48 -0500

    added time to the blog list

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,6 +7,38 @@ export const metadata = {
   description: 'Read my thoughts on software development, design, and more.',
 };
 
+function formatDate(date: string) {
+  let currentDate = new Date();
+  if (!date.includes('T')) {
+    date = `${date}T00:00:00`;
+  }
+  let targetDate = new Date(date);
+
+  let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
+  let monthsAgo = currentDate.getMonth() - targetDate.getMonth();
+  let daysAgo = currentDate.getDate() - targetDate.getDate();
+
+  let formattedDate = '';
+
+  if (yearsAgo > 0) {
+    formattedDate = `${yearsAgo}y ago`;
+  } else if (monthsAgo > 0) {
+    formattedDate = `${monthsAgo}mo ago`;
+  } else if (daysAgo > 0) {
+    formattedDate = `${daysAgo}d ago`;
+  } else {
+    formattedDate = 'Today';
+  }
+
+  let fullDate = targetDate.toLocaleString('en-us', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+
+  return `${fullDate}`;
+}
+
 export default async function BlogPage() {
   let allBlogs = getBlogPosts();
 
@@ -29,6 +61,11 @@ export default async function BlogPage() {
             <div className="w-full flex flex-col">
               <p>{post.metadata.title}</p>
               {/* <ViewCounter slug={post.slug} trackView={false} /> */}
+              <div className="flex justify-between items-center mt-2 mb-2 text-sm max-w-[650px]">
+                <p className="text-sm text-neutral-600 dark:text-neutral-400">
+                  {formatDate(post.metadata.publishedAt)}
+                </p>
+              </div>
             </div>
           </Link>
         ))}
