98f777df9d5a20b7c8e7e5c4a68a2343ce65c359
yuximing <93099324+yuximing@users.noreply.github.com>
2024-01-12 19:51:41 -0500

    cleaned up info page

--- lib/info.tsx
+++ lib/info.tsx
@@ -5,14 +5,15 @@ export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hi! I'm a fourth year Computer Science student at Queen's University. I am currently doing an Co-op at Ciena as a Software Engineer:)
+      Hi! My name is Ximing and I'm a fourth year Computer Science student at Queen's University.
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      When I'm not sitting in front of my nice 34" LG monitor, I'm probably in the kitchen or the gym! I also love to travel and explore new places (especially good food places!).
+      {/* When I'm not sitting in front of my nice 34" LG monitor, I'm probably in the kitchen or the gym! I also love to travel and explore new places (especially good food places!). */}
+      2nd paragraph
     </>
   );
 };
