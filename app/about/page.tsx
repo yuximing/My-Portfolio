8b7cbacbcbc691a5e733f70eb3222c12f4e1557a
yuximing <93099324+yuximing@users.noreply.github.com>
2024-01-13 16:16:52 -0500

    updated home and about page content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,8 +3,25 @@ export default function AboutPage() {
     <section>
       <h1 className="font-bold text-3xl font-serif">About Me</h1>
       <p className="my-5 text-neutral-800 dark:text-neutral-200">
-        This is my about page.
+        Greetings! Here's a closer look at who I am beyond the lines of code:
       </p>
+
+      <h2 className="my-5 font-bold text-xl text-neutral-800 dark:text-neutral-200">The Story So Far:</h2>
+      <p className="my-5 text-neutral-800 dark:text-neutral-200">
+      I began my academic journey with a passion for unraveling the complexities of software design. As I navigated through various interesting courses in computer science, my curiosity only deepened. This curiosity, combined with my self-starter mentality, led me to hands-on experiences as a Software Engineer Co-op at Ciena, where I had the privilege of contributing to impactful projects and refining my skills.
+      </p>
+
+      <h2 className="my-5 font-bold text-xl text-neutral-800 dark:text-neutral-200">Teaching and Mentoring:</h2>
+      <p className="my-5 text-neutral-800 dark:text-neutral-200">
+      Beyond coding, I find immense satisfaction in sharing knowledge. My role as a Teaching Assistant at Queen's University allowed me to guide fellow students through the intricacies of computer science, discrete mathematics, and more. In my role as a Teaching Assistant at Queen's University and a Programming Mentor at Kingston Frontenac Public Library, I excel in simplifying complex technical concepts for individuals with no prior technical background. My approach fosters a collaborative learning atmosphere, creating an inclusive environment where everyone feels empowered to explore the exciting world of programming.
+      </p>
+
+      <h2 className="my-5 font-bold text-xl text-neutral-800 dark:text-neutral-200">Projects That Define Me:</h2>
+      <p className="my-5 text-neutral-800 dark:text-neutral-200">
+      I believe in the power of projects to showcase creativity and refine skills. Some of the interesting projects I have worked on are "Psychokinefish," a 2D side-scrolling arcade game, "Mini Airbnb," a full-stack web application, and "Weatherly," a mobile real-time weather application.
+
+      Recently, I've delved into the exciting world of machine learning with a project building a sign language detector. This exploration reflects my dedication to staying at the forefront of technological advancements. Additionally, I'm actively honing my skills in embedded software programming through upcoming fun projects on Raspberry Pi. Stay tuned for more projects that showcase my passion for continuous learning and hands-on exploration in technologies.
+</p>
     </section>
   );
 }
--- lib/info.tsx
+++ lib/info.tsx
@@ -5,7 +5,7 @@ export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hi! My name is Ximing and I'm a fourth year Computer Science student at Queen's University.
+      Hi! My name is Ximing and I'm a fourth-year Computer Science student with a specialization in Software Design at Queen's University.
     </>
   );
 };
@@ -13,7 +13,7 @@ export const bio = () => {
   return (
     <>
       {/* When I'm not sitting in front of my nice 34" LG monitor, I'm probably in the kitchen or the gym! I also love to travel and explore new places (especially good food places!). */}
-      2nd paragraph
+      I am interested in real-time, embedded software development, backend web development and cloud computing. I also always like to explore the latest in technology, experimenting with new frameworks, and staying curious about emerging trends.
     </>
   );
 };
